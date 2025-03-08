7546d8053ac004759f0b05c6a6ec5a88cd28b57e
Marc <marcaguilar.1803@gmail.com>
2025-03-08 16:26:21 +0100

    Wandb logging

--- Week_1/detr/finetuning.py
+++ Week_1/detr/finetuning.py
@@ -1,10 +1,31 @@
 import torch
-from transformers import AutoImageProcessor, DetrForObjectDetection, TrainingArguments, Trainer
+from transformers import AutoImageProcessor, DetrForObjectDetection, TrainingArguments, Trainer, set_seed
 from data_processing import KITTIMOTS_CocoDetection
-import evaluate
-from tqdm import tqdm
-import numpy as np
 import albumentations
+from dotenv import load_dotenv
+import os
+import wandb
+from transformers import set_seed
+import numpy as np
+
+SEED = 42
+
+# Set seeds for Python, NumPy, and PyTorch
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(SEED)
+    torch.cuda.manual_seed_all(SEED)
+
+# Set seed for Hugging Face transformers (affects some internal RNGs)
+set_seed(SEED)
+
+load_dotenv()
+
+EXPERIMENT_NAME = "KITTI_v2_lower_lr"
+
+WANDB_KEY = os.getenv('WANDB_MARC')
+wandb.login(key=WANDB_KEY)
 
 transform = albumentations.Compose(
     [
@@ -67,26 +88,30 @@ dataset_path = "/home/mcv/datasets/C5/KITTI-MOTS"
 train_instances_ids = [19, 20, 9, 7, 1, 8, 15, 11, 13, 18, 4, 5]
 test_instances_ids = [10, 6, 2, 16, 0, 17, 3, 14, 12]
 
-
 train_dataset = KITTIMOTS_CocoDetection(dataset_path, instances_ids=train_instances_ids, transforms=transform)
 test_dataset = KITTIMOTS_CocoDetection(dataset_path, instances_ids=test_instances_ids)
 print("HF Datasets loaded")
 
-training_args = TrainingArguments(
-    output_dir="checkpoints/first-finetune-detr",
-    per_device_train_batch_size=8,
-    num_train_epochs=1,
-    fp16=True,
-    save_steps=200,
-    logging_steps=10,           # More frequent logging
-    logging_first_step=True,    # Log the very first step
-    logging_dir="logs",
-    learning_rate=1e-5,
-    weight_decay=1e-4,
-    save_total_limit=2,
-    remove_unused_columns=False,
-    report_to=[],               # Disable external logging integrations
-)
+training_config = {
+    "output_dir": f"checkpoints/{EXPERIMENT_NAME}",
+    "per_device_train_batch_size": 8,
+    "num_train_epochs": 20,
+    "fp16": True,
+    "save_steps": 100,
+    "logging_steps": 1,
+    "evaluation_strategy": "steps",  # Evaluate during training
+    "eval_steps": 30,
+    "logging_first_step": True,    # Log the very first step
+    "learning_rate": 5e-6,
+    "weight_decay": 1e-4,
+    "save_total_limit": 2,
+    "remove_unused_columns": False,
+    "report_to": ['wandb'],
+}
+
+wandb.init(name=EXPERIMENT_NAME, project="C5_W1_DETR", config=training_config)
+
+training_args = TrainingArguments(**training_config)
 
 trainer = Trainer(
     model=model,
